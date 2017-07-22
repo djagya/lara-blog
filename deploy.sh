ba4ff0bc8ac74700783742ebb7a4afe4635f0cf8
djagya <danil.kabluk@gmail.com>
2017-07-22 06:19:14 +0200

    lara changes

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+cd "$(dirname "$0")"
+
 if ! git config remote.all.url > /dev/null; then
   git remote add all git@bitbucket.org:djagya/lara-blog.git
   git remote set-url --add all ssh://root@146.185.131.218/var/www/git/lara-blog.git
 fi
 
-sleep 3
 
 git add .
 git commit -m "lara changes"
 git push all --all
 
+sleep 10
