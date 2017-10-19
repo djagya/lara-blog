2e3ce5f8b1551abbd9650ac4c597922160a6ca48
djagya <danil.kabluk@gmail.com>
2017-10-19 22:54:27 +0200

    fix

--- _plugins/images.rb
+++ _plugins/images.rb
@@ -17,7 +17,7 @@ module Jekyll
 
       output = '<div class="images-row">'
       images.each {|img| output += "<div class='images-row__wrapper'>"\
-        "<div class='images-row__img' style=\"background-image:url('#{img.attribute('src').to_s}')\"></div>"\
+        "<div class='images-row__img'><img src='#{img.attribute('src').to_s}'></div>"\
         "</div>"
       }
       output += '</div>'
